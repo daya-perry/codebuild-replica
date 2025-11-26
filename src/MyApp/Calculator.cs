374d2a011d61e06ef4d460fa92585937a9da90a9
daya-perry <dayaper@amazon.com>
2025-11-25 16:04:58 -0800

    Add files via upload

---
+++ src/MyApp/Calculator.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyApp;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+    public int Subtract(int a, int b) => a - b;
+    public int Multiply(int a, int b) => a * b;
+    public int Divide(int a, int b) => b == 0 ? throw new DivideByZeroException() : a / b;
+}
